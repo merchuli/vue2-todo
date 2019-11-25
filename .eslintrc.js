7e32c29e0e90c58ddbde7be70312160d20657be1
merchuli <merchu.liang@gmail.com>
2019-11-25 17:04:50 +0800

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,28 @@ module.exports = {
     'vue',
   ],
   rules: {
+    "indent": ["error", "tab", {
+        "SwitchCase": 1,
+        "flatTernaryExpressions": false
+    }],
+    "no-tabs": "off",
+    "object-curly-spacing": ["error", "never"],
+    "arrow-parens": ["error", "as-needed"],
+    "function-paren-newline": "off",
+    "comma-dangle": ["error", "always-multiline"],
+    "no-param-reassign": ["error", {"props": false}],
+    "max-len": ["error", {"code": 180}],
+    "operator-linebreak": ["error", "after"],
+    "no-underscore-dangle": ["error", {"allow": ["__INIT_STATE__"]}],
+    "import/prefer-default-export": "off",
+    "vue/html-indent": ["error", "tab", {
+        "attribute": 1,
+        "closeBracket": 0,
+        "alignAttributesVertically": true,
+        "ignores": []
+    }],
+    "vue/html-closing-bracket-spacing": ["error", {
+        "selfClosingTag": "always"
+    }],
   },
 };
